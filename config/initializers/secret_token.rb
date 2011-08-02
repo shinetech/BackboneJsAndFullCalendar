e4ed137c82034627c0bad5de3444501995e44807
Ben Teese <ben.teese@shinetech.com>
2011-08-02 16:55:50 +1000

    Initial version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackboneJsAndFullCalendar::Application.config.secret_token = '75d25ec96223a62b2271a80b4739d5677759670ff5453128dd7bf6c47c18b4193fc69978856d36e9bd4ed87a8d2b832502a09f35919d2f8af533682608608a7b'
